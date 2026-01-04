21a1b4f5f4a9e4f6a26833e819cdeaf03f5df425
mtthwryn <matthewrynt@gmail.com>
2026-01-04 17:12:37 +0800

    fix drawer menu for mobile

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,5 @@
 import GithubIcon from '@/components/icons/GithubIcon'
+import LinkedinIcon from '@/components/icons/LinkedinIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
@@ -6,5 +7,10 @@ export const SOCIALNETWORKS = [
 		name: 'Github',
 		url: 'https://github.com/mtthwryn',
 		icon: GithubIcon
+	},
+	{
+		name: 'LinkedIn',
+		url: 'https://linkedin.com/in/mtthwryn',
+		icon: LinkedinIcon
 	}
 ] as const
