1780413d2590d1c526e88fb7602d05a04d5d317f
mtthwryn <matthewrynt@gmail.com>
2026-01-04 15:42:00 +0800

    change categories

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -12,7 +12,7 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	site: 'https://ideal-stack.netlify.app/', // Write here your website url
 	author: 'ideal-stack', // Site author
-	title: 'ideal-stack', // Site title.
+	title: 'Dev Blog', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
